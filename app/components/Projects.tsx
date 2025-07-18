a6fcba6d4ca58df846cd63b53d86d1e5e580dbec
Shafat Alam <61838416+Shafat21@users.noreply.github.com>
2025-07-19 00:35:15 +0600

    Update Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -142,9 +142,9 @@ export default function Projects() {
       title: "Peaky Blinders | Cops and Robbers V Crew Website",
       description: "Join the dominant force in FiveM's Cops and Robbers. From high-stakes heists to law enforcement takeovers, Peaky Blinders plays hard and fair.",
       techStack: "Next.jS, Tailwind, FiveM API, CNRV API",
-      link: "https://peakyblindercnrv.vercel.app/",
+      link: "https://github.com/Shafat21/Peaky-Blinders-Crew-iy",
       icon: <Code className="w-6 h-6 text-lightgray-300" />,
-      type: "live",
+      type: "github",
       category: "Web",
     },
 
